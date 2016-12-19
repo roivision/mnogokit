b648de146d139efd17f805df156e94e307b1e9c8
Aurélien <aurel@gazelle.company>
2016-12-19 10:07:10 -0500

    mongo backup script

--- mngokit/backup.py
+++ mngokit/backup.py
@@ -1,5 +1,27 @@
+from time import sleep
 import click
-from sh import mongodump, s3fs, fusermount
+import os
+import sys
+import sh
+from datetime import datetime
+
+
+for module in ['mongodump', 's3fs', 'fusermount', 'sync']:
+    try:
+        globals()[module] = getattr(sh, module)
+    except:
+        print("{} is not installed".format(module))
+        raise
+
+bucket = 'gazelle-mongobackups'
+environment = 'dev'
+mountpoint = '~/mongobackups'
+timestamp = datetime.now().strftime('%Y-%m-%d %H%M')
+
+def read_config():
+    # open config file
+    # get variables or ENV.get(x, default)
+    pass
 
 
 @click.command(
@@ -8,35 +30,43 @@ from sh import mongodump, s3fs, fusermount
             allow_extra_args=True,
     ))
 @click.option('--collections', '-c', required=False)
-@click.option('--destination', '-o', default="/tmp/")
 @click.option('--message', '-m', required=False)
 @click.pass_context
-def parse(ctx, collections, destination, message):
-    if collections:
-        collections = collections.split(",")
-        for collection in collections:
+def run(ctx, collections, message):
+    expanded_mountpoint = os.path.expanduser(mountpoint)
+    expanded_mountpoint = os.path.expandvars(expanded_mountpoint)
+    if not os.path.exists(expanded_mountpoint):
+        os.makedirs(expanded_mountpoint)
+    try:
+        s3fs([bucket, expanded_mountpoint])
+    except:
+        print("""configure s3fs password echo MYIDENTITY:MYCREDENTIAL >
+        /etc/passwd-s3fs
+        chmod 600 /etc/passwd-s3fs""")
+        raise
+    destination =  os.path.join(expanded_mountpoint, environment, timestamp)
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    try:
+        if collections:
+            collections = collections.split(",")
+            for collection in collections:
+                mongodump(
+                        "--collection", collection,
+                        "-o", destination,
+                        *ctx.args)
+            return
+        else:
             mongodump(
-                    "--collection", collection,
                     "-o", destination,
                     *ctx.args)
-        return
-    else:
-        mongodump(
-                "-o", destination,
-                *ctx.args)
-        return
-
-def mount(bucket, mountpoint):
-    s3fs(bucket, mountpoint)
-
-def umount(mountpoint):
-    fusermount('-u', mountpoint)
+            return
+    finally:
+        sync()
+        sleep(10)
+        fusermount('-u', expanded_mountpoint)
 
 
 if __name__ == '__main__':
-    print("""install s3fs: apt-get install s3fs""")
-    print("""configure s3fs password echo MYIDENTITY:MYCREDENTIAL >
-    /etc/passwd-s3fs
-    chmod 600 /etc/passwd-s3fs""")
-    parse()
+    run()
 
--- mngokit/test_backup.py
+++ mngokit/test_backup.py
@@ -1,21 +1,21 @@
 from mock import patch, call
 from unittest import TestCase
-from backup import parse
+from backup import run
 from click.testing import CliRunner
 
 
 @patch('backup.mongodump')
 class TestDump(TestCase):
 
-    def test_parser(self, mongodump):
+    def test_runner(self, mongodump):
         runner = CliRunner()
         result = runner.invoke(
-                parse,
+                run,
                 ['-d', 'database', '--collections', 'c1,c2', '-o', '/tmp/'])
         mongodump.assert_called_with(
                 '--collection', u'c2', '-o', u'/tmp/', '-d', 'database')
         result = runner.invoke(
-                parse,
+                run,
                 ['-d', 'database', '-o', '/tmp/'])
         mongodump.assert_called_with(
                 '-o', u'/tmp/', '-d', 'database')
