9ef15338c19dfebd6462c3493fb26780fc7e033e
Aurélien <aurel@gazelle.company>
2016-12-19 14:08:03 -0500

    read ~/.mnogokit config file

--- mngokit/backup.py
+++ mngokit/backup.py
@@ -3,6 +3,7 @@ import click
 import os
 import sys
 import sh
+import ConfigParser
 from datetime import datetime
 
 
@@ -13,15 +14,33 @@ for module in ['mongodump', 's3fs', 'fusermount', 'sync']:
         print("{} is not installed".format(module))
         raise
 
-bucket = 'gazelle-mongobackups'
-environment = 'dev'
-mountpoint = '~/mongobackups'
-timestamp = datetime.now().strftime('%Y-%m-%d %H%M')
+config_section = 'mnogokit'
+defaults = {
+    "bucket": 'gazelle-mongobackups',
+    "environment": 'dev',
+    "mountpoint": '~/mongobackups',
+    "config_path": '~/.mnogokit',
+    "timestamp": datetime.now().strftime('%Y-%m-%d-%H%M')
+}
 
-def read_config():
-    # open config file
-    # get variables or ENV.get(x, default)
-    pass
+def read_config(config_file):
+    config_file = expand_path(config_file)
+    config = ConfigParser.SafeConfigParser(defaults)
+    config.read(config_file)
+    if not config.has_section(config_section):
+        config.add_section(config_section)
+    for setting in defaults:
+        config.set(
+                config_section,
+                setting, 
+                os.environ.get(setting.upper())
+                or config.get(config_section, setting))
+    return config
+
+def expand_path(path):
+    expanded_path = os.path.expanduser(path)
+    expanded_path = os.path.expandvars(expanded_path)
+    return expanded_path
 
 
 @click.command(
@@ -31,10 +50,17 @@ def read_config():
     ))
 @click.option('--collections', '-c', required=False)
 @click.option('--message', '-m', required=False)
+@click.option('--config-file', '-C', required=False)
 @click.pass_context
-def run(ctx, collections, message):
-    expanded_mountpoint = os.path.expanduser(mountpoint)
-    expanded_mountpoint = os.path.expandvars(expanded_mountpoint)
+def run(ctx, collections, message, config_file):
+
+    config = read_config(config_file or defaults.get('config_path'))
+    bucket = config.get(config_section, 'bucket')
+    environment = config.get(config_section, 'environment')
+    mountpoint = config.get(config_section, 'mountpoint')
+    timestamp = config.get(config_section, 'timestamp')
+
+    expanded_mountpoint = expand_path(mountpoint)
     if not os.path.exists(expanded_mountpoint):
         os.makedirs(expanded_mountpoint)
     try:
@@ -53,14 +79,16 @@ def run(ctx, collections, message):
             for collection in collections:
                 mongodump(
                         "--collection", collection,
-                        "-o", destination,
+                        "-o", '"{}"'.format(destination),
                         *ctx.args)
-            return
         else:
             mongodump(
-                    "-o", destination,
+                    "-o", '"{}"'.format(destination),
                     *ctx.args)
-            return
+        if message:
+            commit_file = os.path.join(destination, 'COMMIT')
+            with open(commit_file, 'wb') as f:
+                f.write(message)
     finally:
         sync()
         sleep(10)
