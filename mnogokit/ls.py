2a0ef9525f269b84a1056fc6667b148d2cd92583
Aurélien <aurel@gazelle.company>
2016-12-20 10:49:51 -0500

    ls shows commit message if available

--- mnogokit/ls.py
+++ mnogokit/ls.py
@@ -1,6 +1,7 @@
 import click
 import os
 from mnogokit import pass_conf, mount_s3
+import boto3
 
 @click.command(
         context_settings=dict(
@@ -9,8 +10,21 @@ from mnogokit import pass_conf, mount_s3
     ))
 @pass_conf
 def ls(config):
-    with mount_s3(config.bucket, config.mountpoint) as mounted:
-        env_dir = os.path.join(mounted, config.environment)
-        print(os.listdir(env_dir))
+    client = boto3.client('s3')
+    paginator = client.get_paginator('list_objects')
+    # https://github.com/boto/boto3/blob/develop/boto3/examples/s3.rst#list-top-level-common-prefixes-in-amazon-s3-bucket
+    for result in paginator.paginate(
+            Bucket=config.bucket,
+            Delimiter='/',
+            Prefix='{}/'.format(config.environment)):
+        for prefix in result.get('CommonPrefixes'):
+            prefix_value = prefix.get('Prefix') 
+            try:
+                commit_msg = client.get_object(
+                        Bucket=config.bucket,
+                        Key=prefix_value.join('COMMIT'))
+            except:
+                commit_msg = "No commit message"
+            print(': '.join([prefix_value, commit_msg]))
 
 
